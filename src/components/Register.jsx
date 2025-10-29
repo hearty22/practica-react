cc5c0c60783147700d7036012ac0aef8cfa032a6
hearty_dev <facudnoesclante@gmail.com>
2025-10-29 14:49:18 -0300

    estilo base

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
+import "./Register.css"
 export const Register = () => {
+
   return (
     <div className='RegisterContainer'>
-        <form action="">
-            <h2>Registro</h2>
+        <form>
+            <h2 className="formTitle">Register</h2>
+            <label htmlFor="username">username</label> <br />
+            <input type="text" id="username" name='username'/>
+            <br />
+            <label htmlFor="username">password</label> <br />
+            <input type="text" id="password" name='password'/>
+            <br /><br />
+            <button>submit</button>
         </form>
     </div>
   )
-}
+};
