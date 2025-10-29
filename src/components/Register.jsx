c9bc74848f6b7d7d9ca25cad0e64e7d98b5d736b
hearty_dev <facudnoesclante@gmail.com>
2025-10-29 12:45:29 -0300

    react base

---
+++ src/components/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Register = () => {
+  return (
+    <div className='RegisterContainer'>
+        <form action="">
+            <h2>Registro</h2>
+        </form>
+    </div>
+  )
+}
