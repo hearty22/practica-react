d0d69b40c1a4ecc3d65178c76b51c9ec243649f4
hearty_dev <facudnoesclante@gmail.com>
2025-11-02 19:21:16 -0300

    .

--- Practica-3/Practica-3/src/hooks/useFetch.js
+++ Practica-3/Practica-3/src/hooks/useFetch.js
@@ -1,51 +1,46 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export const useFetch =  (value) => {
+export const useFetch = (value) => {
+  const [characterData, setCharacterData] = useState({
+    name: "",
+    gender: "",
+    age: "",
+    occupation: "",
+  });
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
-    const [characterData, setCharacterData] = useState({
-        name: "",
-        gender: "",
-        age: "",
-        occupation: "",
-    });
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      setHasError(false);
+      try {
+        const url = "https://thesimpsonsapi.com/api";
+        const response = await fetch(`${url}/characters/${value}`);
 
+        const data = await response.json();
+        // setName(data.name);
+        // setGender(data.gender);
+        setCharacterData({
+          name: data.name,
+          gender: data.gender,
+          age: data.age,
+          occupation: data.occupation,
+        });
+      } catch (error) {
+        console.error(error);
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getData();
+  }, [value]);
 
-
-    const [isLoading, setIsLoading] = useState(true);
-    const [hasError, setHasError] = useState(false);
-
-    useEffect( ()=>{
-        const getData = async () => {
-            setIsLoading(true);
-            setHasError(false);
-            try {
-                const url = "https://thesimpsonsapi.com/api"
-                const response = await fetch(`${url}/characters/${value}`);
-                
-
-                const data = await response.json();
-                // setName(data.name);
-                // setGender(data.gender);
-                setCharacterData({
-                    name: data.name,
-                    gender: data.gender,
-                    age: data.age,
-                    occupation: data.occupation,
-                })
-            } catch (error) {
-                console.error(error);
-                setHasError(true);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        getData();
-    },[value]) 
-    
-    return{
-        characterData,
-        isLoading,
-        hasError
-    }
-}
+  return {
+    characterData,
+    isLoading,
+    hasError,
+  };
+};
--- Practica-3/Practica-3/src/pages/App.jsx
+++ Practica-3/Practica-3/src/pages/App.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import './App.css';
-import { CharacterInfo } from '../components/CharacterInfo';
+import React from "react";
+import "./App.css";
+import { CharacterInfo } from "../components/CharacterInfo";
 
-export const  App =() => {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Los simpson API en React</h1>
       </header>
       <main className="App-main-content">
-        <CharacterInfo/>
-        <p>||
-          Contenido principal.
-        </p>
+        <CharacterInfo />
+        <p>|| Contenido principal.</p>
       </main>
     </div>
   );
-}
-
+};
